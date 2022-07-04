7e66ee1ab1ca4d240d60ebb2d8ca2b1f498cda64
Gustavo Batista <gustavobatistafig@hotmail.com>
2022-07-04 19:21:35 -0300

    Salvando Restaurantes Recebidos

--- marketplace/src/main/java/com/github/gustavobf/ifood/mp/Restaurante.java
+++ marketplace/src/main/java/com/github/gustavobf/ifood/mp/Restaurante.java
@@ -7,20 +7,30 @@ import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Row;
 import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
 
 public class Restaurante {
-	
+
 	public Long id;
-	
+
 	public String nome;
-	
+
 	public Localizacao localizacao;
 
-	public static Multi<RestauranteDTO> findAll(PgPool pgPool) {
-		 Uni<RowSet<Row>> preparedQuery = pgPool.query("SELECT * FROM restaurante").execute();
-	        return preparedQuery.onItem().transformToMulti(rowSet -> Multi.createFrom().items(() -> {
-	        	return StreamSupport.stream(rowSet.spliterator(), false);
-	        })).onItem().transform(RestauranteDTO::from); 
+	public static Multi<RestauranteDTO> findAll(final PgPool pgPool) {
+		final Uni<RowSet<Row>> preparedQuery = pgPool.query("SELECT * FROM restaurante").execute();
+		return preparedQuery.onItem().transformToMulti(rowSet -> Multi.createFrom().items(() -> {
+			return StreamSupport.stream(rowSet.spliterator(), false);
+		})).onItem().transform(RestauranteDTO::from);
 	}
-	
+
+	public void persist(final PgPool pgPool) {
+		pgPool.preparedQuery("insert into localizacao (id, latitude, longitude) values ($1, $2, $3)").execute(
+				Tuple.of(localizacao.id, localizacao.latitude, localizacao.longitude)).await().indefinitely();
+
+		pgPool.preparedQuery("insert into restaurante (id, nome, localizacao_id) values ($1, $2, $3)").execute(
+				Tuple.of(id, nome, localizacao.id)).await().indefinitely();
+
+	}
+
 }
--- marketplace/src/main/java/com/github/gustavobf/ifood/mp/RestauranteCadastrado.java
+++ marketplace/src/main/java/com/github/gustavobf/ifood/mp/RestauranteCadastrado.java
@@ -1,20 +1,27 @@
 package com.github.gustavobf.ifood.mp;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import io.vertx.mutiny.pgclient.PgPool;
+
 @ApplicationScoped
-public class RestautanteCadastrado {
+public class RestauranteCadastrado {
+
+	@Inject
+	PgPool pgPool;
 
 	@Incoming("restaurantes")
 	public void receberRestaurante(final String json) {
 		final Jsonb create = JsonbBuilder.create();
 		final Restaurante restaurante = create.fromJson(json, Restaurante.class);
-		System.out.println(json);
-		System.out.println(restaurante);
+
+		restaurante.persist(pgPool);
+
 	}
 
 }
