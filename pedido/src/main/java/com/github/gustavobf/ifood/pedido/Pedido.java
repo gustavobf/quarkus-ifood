34899a800afb5ebcc73b32347e47460df74c958d
Gustavo Batista <gustavobatistafig@hotmail.com>
2022-07-14 19:47:52 -0300

    Salvando Pedidos Recebidos no MongoDB

--- pedido/src/main/java/com/github/gustavobf/ifood/pedido/Pedido.java
+++ pedido/src/main/java/com/github/gustavobf/ifood/pedido/Pedido.java
@@ -2,16 +2,17 @@ package com.github.gustavobf.ifood.pedido;
 
 import java.util.List;
 
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 
 @MongoEntity(collection = "pedidos", database = "pedido")
-public class Pedido {
+public class Pedido extends PanacheMongoEntity {
 
 	public String cliente;
 
 	public List<Prato> pratos;
 
-	public Restaurante Restaurante;
+	public Restaurante restaurante;
 
 	public String entregador;
 
--- pedido/src/main/java/com/github/gustavobf/ifood/pedido/PedidoRealizadoIncoming.java
+++ pedido/src/main/java/com/github/gustavobf/ifood/pedido/PedidoRealizadoIncoming.java
@@ -1,7 +1,10 @@
 package com.github.gustavobf.ifood.pedido;
 
+import java.util.ArrayList;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import org.bson.types.Decimal128;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 
@@ -11,6 +14,24 @@ public class PedidoRealizadoIncoming {
 	@Incoming("pedidos")
 	public void lerPedidos(final PedidoRealizadoDTO dto) {
 		System.out.println(dto);
+
+		final Pedido pedido = new Pedido();
+		pedido.cliente = dto.cliente;
+		pedido.pratos = new ArrayList<Prato>();
+
+		dto.pratos.forEach(prato -> pedido.pratos.add(this.from(prato)));
+		final Restaurante restaurante = new Restaurante();
+		restaurante.nome = dto.restaurante.nome;
+		pedido.restaurante = restaurante;
+		pedido.persist();
+	}
+
+	private Prato from(final PratoPedidoDTO prato) {
+		final Prato p = new Prato();
+		p.descricao = prato.descricao;
+		p.nome = prato.nome;
+		p.preco = new Decimal128(prato.preco);
+		return p;
 	}
 
 }
